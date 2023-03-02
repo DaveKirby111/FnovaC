71caf1583b1d6de56948ee789cc0fca99798bffb
David Kirby <david@kinetic.com>
2023-03-02 15:46:21 -0600

    added plugins and styling

--- wp-content/themes/fnc/footer.php
+++ wp-content/themes/fnc/footer.php
@@ -1,4 +1,29 @@
-<footer></footer>
+<footer>
+
+<div class="contact">
+
+<?php
+
+wp_nav_menu(
+
+    array(
+
+       'theme_location' => 'footer-menu',
+        'container_class' => 'footer-nav'
+    )
+
+);
+
+?>
+
+</div>
+
+<div class="info">
+    <p>Author: David Kirby 2023</p>
+    <p>Date: <?php echo date('M d, Y'); ?></p>
+</div>
+
+</footer>
 <?php wp_footer() ?>
 </body>
 </html>
